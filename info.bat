3e47dad4961e87a9dc16c1a7ea438266267d4947
Niquill0 <85759792+nicovarvara@users.noreply.github.com>
2024-11-06 17:51:37 -0300

    Update info.bat

--- info.bat
+++ info.bat
@@ -1,11 +1,15 @@
 @echo off
 setlocal
-
-start cmd /k "(
-wmic useraccount get /ALL /format:csv > info.txt &&
-wmic process get caption,executablepath,commandline /format:csv >> info.txt &&
+wmic useraccount get /ALL /format:csv > info.txt
+wmic process get caption,executablepath,commandline /format:csv >> info.txt
 wmic qfe get description,hotfixid,installedOn,ServicePackInEffect /format:csv >> info.txt
-")
+wmic cup get name >> info.txt
+wmic MEMPHYSICAL get MaxCapacity >> info.txt
+wmic baseboard get product >> info.txt
+wmic baseboard get version >> info.txt
+wmic bios get SMBIOSBIOSVersion >> info.txt
+wmic OS get Caption,OSArchitecture,Version >> info.txt
+wmic DISKDRIVE get Caption >> info.txt
 echo "finished"
 timeout 5 
 endlocal
