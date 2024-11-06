c07f40344a26737afd9f4a6717a06eb67953da9e
Niquill0 <85759792+nicovarvara@users.noreply.github.com>
2024-11-06 17:47:14 -0300

    Update info.bat

--- info.bat
+++ info.bat
@@ -5,8 +5,7 @@ start cmd /k "(
 wmic useraccount get /ALL /format:csv > info.txt &&
 wmic process get caption,executablepath,commandline /format:csv >> info.txt &&
 wmic qfe get description,hotfixid,installedOn,ServicePackInEffect /format:csv >> info.txt
-
+")
 echo "finished"
 timeout 5 
-")
 endlocal
